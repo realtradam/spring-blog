2f91d2067e7da107a83225f063216c0c6dd7b7cc
realtradam <github@tradam.dev>
2024-07-13 04:23:01 -0400

    complete registration and login

--- src/main/java/com/blog/web/services/impl/UserServiceImpl.java
+++ src/main/java/com/blog/web/services/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.blog.web.models.UserEntity;
 import com.blog.web.repository.RoleRepository;
 import com.blog.web.repository.UserRepository;
 import com.blog.web.services.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -14,10 +15,15 @@ import java.util.Arrays;
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+    public UserServiceImpl(
+            UserRepository userRepository,
+            RoleRepository roleRepository,
+            PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -27,8 +33,9 @@ public class UserServiceImpl implements UserService {
         user.setEmail(registrationDto.getEmail());
         // this is an unsafe way to store passwords in production
         // it is left this way only because this is a practice project
-        user.setPassword(registrationDto.getPassword());
-        
+        //user.setPassword(registrationDto.getPassword());
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
+
         Role role = roleRepository.findByName("User");
         user.setRoles(Arrays.asList(role));
         userRepository.save(user);
